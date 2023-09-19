029a7815656818130bfc2cf889be8691f7b61133
amaro23vic <145304738+amaro23vic@users.noreply.github.com>
2023-09-19 11:20:42 -0600

    initial commit

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function verificarCredenciales() {
     const password = document.getElementById('password').value;
 
     // Verifica las credenciales hardcodeadas (esto es solo para pruebas)
-    if (username === 'VictorA28' && password === 'amaro23vic') {
+    if (username === 'VictorA28' && password === 'victordaniel2001*') {
         // Credenciales válidas, muestra el contenido principal
         document.getElementById('login-container').style.display = 'none';
         document.getElementById('container').style.display = 'block';
